27d87830b82264b6416034ce453cc04a3fe35d79
Helton Soares <hgsdl@live.com>
2025-11-15 14:28:30 -0300

    Primeiro commit

---
+++ src/main/java/com/heltonaleper/pr_legislativo/PrLegislativoApplication.java
@@ -0,0 +1,13 @@
+package com.heltonaleper.pr_legislativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrLegislativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrLegislativoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heltonaleper/pr_legislativo/PrLegislativoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heltonaleper.pr_legislativo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrLegislativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
